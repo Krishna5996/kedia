4057c9e1337e44be8341ff55cab66ff9dda4957e
Krishna5996 <krishnajadhav5996@gmail.com>
2022-11-23 17:42:15 +0530

    js practice

---
+++ app.js
@@ -0,0 +1,130 @@
+/*const amount=12
+if (amount<10){
+    console.log('small number')
+}
+
+else
+{
+    console.log('large number')
+
+}
+
+console.log('its my first app')*/
+
+
+
+
+/*
+console.log(__dirname)
+
+
+setInterval(()=>{console.log('hello world')},1000)
+
+
+
+const firstname='krishna'
+const lastname='jadhav'
+
+const sayhi=(name)=>
+{
+    console.log(`hello ${name}`);
+
+
+}
+
+sayhi(firstname)
+
+
+
+
+
+var a=10;
+function funa()
+{
+    console.log("in a");
+    function funb()
+    {
+        console.log("in b");
+    
+    }
+    funb();
+}
+
+funa();
+
+console.log("globalcontext");
+
+
+
+var a=10;
+function getname()
+{
+console.log("hi krishna")
+}
+console.log(a);
+getname();
+//console.log(getname)
+
+
+
+var x=1;
+a();
+b();
+console.log(x);
+function a(){
+    x=10;
+    console.log(x);
+}
+function b(){
+    x=100;
+    console.log(x);
+}
+
+var message='hi';
+console.log(message);
+
+var  a=10;
+//console.log(a);
+var b=100;
+function sample(a,b) {
+    console.log(a);
+    console.log(a+b);
+}
+sample();
+
+
+{
+    var c =30; 
+    let b=20; 
+    const a=10;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
